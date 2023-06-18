06231fb716654f999ffe53ce230289ab45d427e9
Manthan Ankolekar <manthan.ank@gmail.com>
2023-06-18 22:03:01 +0530

    added firebase config

--- angularfire/src/environments/environment.prod.ts
+++ angularfire/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-    production: true
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyC9OsfVdm4MX87daetXWyxvSYMxeSuLOOc",
+        authDomain: "angular-firebase-angularfire.firebaseapp.com",
+        projectId: "angular-firebase-angularfire",
+        storageBucket: "angular-firebase-angularfire.appspot.com",
+        messagingSenderId: "398670920785",
+        appId: "1:398670920785:web:64777b4d9f204c20964c70",
+        measurementId: "G-F0CLXJHX2P"
+    },
 };
