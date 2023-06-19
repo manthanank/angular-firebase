357fcb3a5b23e163c554a60c897797deb5f7378a
Manthan Ankolekar <manthan.ank@gmail.com>
2023-06-19 15:59:59 +0530

    added firebase

--- angularfire/src/environments/environment.ts
+++ angularfire/src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
-    production: false,
-    firebase: {
-        apiKey: "AIzaSyC9OsfVdm4MX87daetXWyxvSYMxeSuLOOc",
-        authDomain: "angular-firebase-angularfire.firebaseapp.com",
-        projectId: "angular-firebase-angularfire",
-        storageBucket: "angular-firebase-angularfire.appspot.com",
-        messagingSenderId: "398670920785",
-        appId: "1:398670920785:web:64777b4d9f204c20964c70",
-        measurementId: "G-F0CLXJHX2P"
-    },
+  production: false,
+  firebase: {
+    projectId: 'angular-firebase-angularfire',
+    appId: '1:398670920785:web:64777b4d9f204c20964c70',
+    storageBucket: 'angular-firebase-angularfire.appspot.com',
+    apiKey: 'AIzaSyC9OsfVdm4MX87daetXWyxvSYMxeSuLOOc',
+    authDomain: 'angular-firebase-angularfire.firebaseapp.com',
+    messagingSenderId: '398670920785',
+    measurementId: 'G-F0CLXJHX2P',
+  },
 };
