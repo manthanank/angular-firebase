bda9b6aa23291a268a6a5c054f7302b0dd50ee71
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-02-05 10:11:01 +0530

    feat: updated

--- rest-api/src/app/app.component.spec.ts
+++ rest-api/src/app/app.component.spec.ts
@@ -1,16 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'rest-api'`, () => {
+  it(`should have the 'rest-api' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('rest-api');
@@ -30,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('rest-api app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, rest-api');
   });
 });
